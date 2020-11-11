68b2e79dcc5743563a0b176f3243f1da54966ff0
M. Taufiq Hidayat <mtaufiqh62@gmail.com>
2020-11-11 16:52:05 +0800

    setup list item terbaru

--- app/src/main/java/id/taufiq/lostandfound/HomeActivity.kt
+++ app/src/main/java/id/taufiq/lostandfound/HomeActivity.kt
@@ -2,24 +2,32 @@ package id.taufiq.lostandfound
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import id.taufiq.lostandfound.adapter.CategoryAdapter
+import id.taufiq.lostandfound.adapter.TerbaruAdapter
 import id.taufiq.lostandfound.helper.showToast
 import id.taufiq.lostandfound.model.local.DataCategory
+import id.taufiq.lostandfound.model.local.DataTerbaru
 import kotlinx.android.synthetic.main.activity_home.*
 
 class HomeActivity : AppCompatActivity() {
     private val data = ArrayList<DataCategory>()
+    private val dataTerbaru = ArrayList<DataTerbaru>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
 
-        listOfData()
-        initRecyclerView()
+        // dummy data
+        dataCategory()
+        dataTerbaru()
+
+        initListCategory()
+        initListTerbaru()
     }
 
-    private fun initRecyclerView() {
+    private fun initListCategory() {
         rv_category.layoutManager = LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false)
         rv_category.adapter = CategoryAdapter(this,data){
             showToast("clicked ${it.title}")
@@ -27,7 +35,16 @@ class HomeActivity : AppCompatActivity() {
     }
 
 
-    private fun listOfData() {
+
+    private fun initListTerbaru() {
+        rv_terbaru.layoutManager = GridLayoutManager(this,2)
+        rv_terbaru.adapter = TerbaruAdapter(this,dataTerbaru){
+
+        }
+    }
+
+
+    private fun dataCategory() {
         data.add(
             DataCategory(
                 CategoryAdapter.VIEW_TYPE_ONE, "Elektronik", R.drawable.image_elektronic
@@ -48,6 +65,42 @@ class HomeActivity : AppCompatActivity() {
 
 
         data.subList(0,3)
+
+
+    }
+
+    private fun dataTerbaru() {
+        dataTerbaru.add(
+            DataTerbaru(
+                R.drawable.tas,"6 November 2020", "Tas Orange",R.drawable.ic_image_hilang,
+                "Barang","Gucci","Orange",resources.getString(R.string.lorem_ipsum)
+            )
+        )
+
+
+        dataTerbaru.add(
+            DataTerbaru(
+                R.drawable.hp,"7 November 2020", "Hp Iphone",R.drawable.ic_image_ketemu,
+                "Elektronik","Iphone","Hitam",resources.getString(R.string.lorem_ipsum)
+            )
+        )
+
+
+        dataTerbaru.add(
+            DataTerbaru(
+                R.drawable.tas,"6 November 2020", "Tas Orange",R.drawable.ic_image_hilang,
+                "Barang","Gucci","Orange",resources.getString(R.string.lorem_ipsum)
+            )
+        )
+
+
+        dataTerbaru.add(
+            DataTerbaru(
+                R.drawable.hp,"7 November 2020", "Hp Iphone",R.drawable.ic_image_ketemu,
+                "Elektronik","Iphone","Hitam",resources.getString(R.string.lorem_ipsum)
+            )
+        )
+
 
 
     }
--- app/src/main/java/id/taufiq/lostandfound/adapter/TerbaruAdapter.kt
+++ app/src/main/java/id/taufiq/lostandfound/adapter/TerbaruAdapter.kt
@@ -6,32 +6,44 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import id.taufiq.lostandfound.R
+import id.taufiq.lostandfound.model.local.DataTerbaru
+import kotlinx.android.synthetic.main.row_item_terbaru.view.*
 
 /**
  * Created By Taufiq on 11/11/2020.
  *
  */
 
-class TerbaruAdapter(context: Context, listener: () -> Unit) : RecyclerView.Adapter<TerbaruAdapter.TerbaruViewHolder>() {
+class TerbaruAdapter(
+    val context: Context,
+    val data: List<DataTerbaru>,
+    val listener: (DataTerbaru) -> Unit,
+) : RecyclerView.Adapter<TerbaruAdapter.TerbaruViewHolder>() {
 
 
     class TerbaruViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        fun binding(context: Context, listener: ()-> Unit){}
+        fun binding(data: DataTerbaru, context: Context, listener: (DataTerbaru) -> Unit) {
+            with(itemView) {
+                tv_item.text = data.itemName
+                tv_tanggal.text = data.tanggal
+                iv_item.setImageResource(data.imageItem)
+                iv_status.setImageResource(data.status)
+
+                setOnClickListener { listener(data) }
+            }
+        }
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TerbaruViewHolder {
-        val view =
-            LayoutInflater.from(parent.context).inflate(R.layout.row_item_category, parent, false)
-
-        return TerbaruViewHolder(view)
+        return TerbaruViewHolder(LayoutInflater.from(parent.context)
+            .inflate(R.layout.row_item_terbaru, parent, false))
     }
 
     override fun onBindViewHolder(holder: TerbaruViewHolder, position: Int) {
-        TODO("Not yet implemented")
+        holder.binding(data[position], context, listener)
     }
 
-    override fun getItemCount(): Int {
-        TODO("Not yet implemented")
-    }
+    override fun getItemCount(): Int = data.size
+
 
 }
